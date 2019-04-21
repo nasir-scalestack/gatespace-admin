99671e3625e622d7b0f56fdbf14028aeed21b5f1
Nasir Dowlatkhahi <ndowlat@Nasirs-MacBook-Air.local>
2019-04-21 18:54:36 -0400

    API Key generation

--- src/components/ProjectAdd.js
+++ src/components/ProjectAdd.js
@@ -160,6 +160,26 @@ class ProjectAdd extends Component {
     return 'Next';
   }
 
+  generateUUID()
+    {
+        var d = new Date().getTime();
+        
+        if( window.performance && typeof window.performance.now === "function" )
+        {
+            d += performance.now();
+        }
+        
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c)
+        {
+            var r = (d + Math.random()*16)%16 | 0;
+            d = Math.floor(d/16);
+            return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+        });
+
+    return uuid;
+    }
+
+
   goToDashboard = event => {
     const queryString = this.props.location.search
 
@@ -393,7 +413,7 @@ PLEASE NOTE: We reserve the right, at our sole discretion, to change, modify or 
                             id="apikey"
                             label="API KEY"
                             type="text"
-                            value='--'
+                            value={this.generateUUID()}
                             fullWidth
                           />
                         </Grid>
