a36c66c6db449de5215d54a892481e785634dc5a
Nasir Dowlatkhahi <ndowlat@Nasirs-MacBook-Air.local>
2019-04-21 18:48:27 -0400

    Project add wizard add

--- src/components/ProjectAdd.js
+++ src/components/ProjectAdd.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { withRouter } from 'react-router-dom'
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -19,6 +21,9 @@ import Select from '@material-ui/core/Select';
 import Back from './common/Back'
 import TextField from '@material-ui/core/TextField';
 import Zones from './Zones';
+
+import { setActiveApp } from '../redux/modules/user';
+
 
 const qs = require('query-string');
 const backgroundShape = require('../images/shape.svg');
@@ -115,10 +120,6 @@ class ProjectAdd extends Component {
     name: null
   }
 
-  componentDidMount() {
-
-  }
-
   handleNext = () => {
     this.setState(state => ({
       activeStep: state.activeStep + 1,
@@ -153,6 +154,7 @@ class ProjectAdd extends Component {
       return 'Install';
     }
     if(this.state.activeStep === 5) {
+      this.props.setActiveApp('app-id','new-app')
       return 'Done';
     }
     return 'Next';
@@ -261,52 +263,29 @@ class ProjectAdd extends Component {
                       <div className={classes.topInfo}>
                         <div>
                           <Typography variant="subtitle1" style={{fontWeight: 'bold'}} gutterBottom>
-                            Details
+                            Confirm Details
                           </Typography>
                           <Typography variant="body1" gutterBottom>
-                            We need some details about any information
+                            Please review the details of your project
                           </Typography>
-                        </div>
-                        <div>
-                          <Button variant="outlined" size="large" className={classes.outlinedButtom}>
-                            Edit
-                          </Button>
                         </div>
                       </div>
                       <div className={classes.borderColumn}>
                         <Grid item container xs={12} style={{marginBottom: 32}}>
                           <Grid item xs={6}>
                             <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
-                              Amount
+                              Project Name
                             </Typography>
                             <Typography variant="h5" gutterBottom>
-                              { parsed ? numeral(parsed.amount).format() : '75,000'} DKK
+                             --
                             </Typography>
                           </Grid>
                           <Grid item xs={6}>
                             <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
-                              Total fees
+                              Project Id
                             </Typography>
                             <Typography variant="h5" gutterBottom>
-                              0 DKK
-                            </Typography>
-                          </Grid>
-                        </Grid>
-                        <Grid item container xs={12}>
-                          <Grid item xs={6}>
-                            <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
-                              Total price
-                            </Typography>
-                            <Typography variant="h5" gutterBottom>
-                              { parsed ? numeral(parsed.interest).format() : '6,600'} USD
-                            </Typography>
-                          </Grid>
-                          <Grid item xs={6}>
-                            <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
-                              Total cost
-                            </Typography>
-                            <Typography variant="h5" gutterBottom>
-                              { parsed ? numeral(parsed.cost).format() : '81,600'} USD
+                              --
                             </Typography>
                           </Grid>
                         </Grid>
@@ -315,34 +294,18 @@ class ProjectAdd extends Component {
                         <Grid item container xs={12} style={{marginBottom: 32}}>
                           <Grid item xs={6}>
                             <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
-                              How often
+                              Zones Selected
                             </Typography>
                             <Typography variant="h5" gutterBottom>
-                              Once a month
+                              --
                             </Typography>
                           </Grid>
-                        </Grid>
-                        <Grid item xs={6}>
-                          <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
-                            When to start
-                          </Typography>
-                          <Typography variant="h5" gutterBottom>
-                            01 February 2019
-                          </Typography>
-                        </Grid>
-                        <Grid item xs={6}>
-                          <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
-                            When it ends?
-                          </Typography>
-                          <Typography variant="h5" gutterBottom>
-                            01 May 2019
-                          </Typography>
                         </Grid>
                       </Grid>
                       <Grid item container xs={12} style={{marginTop: 24}}>
                         <Grid item xs={6}>
                           <Typography style={{textTransform: 'uppercase', marginBottom: 20}} color='secondary' gutterBottom>
-                            Destination account
+                            Billing account
                           </Typography>
                           <FormControl variant="outlined" className={classes.formControl}>
                             <Select
@@ -419,11 +382,20 @@ PLEASE NOTE: We reserve the right, at our sole discretion, to change, modify or 
                       <Grid item container xs={12}>
                         <Grid item xs={12}>
                           <Typography variant="subtitle1" style={{fontWeight: 'bold'}} gutterBottom>
-                            Sign & confirm
+                            Project Added
                           </Typography>
                           <Typography variant="body1" gutterBottom>
-                            Sign and confirm your agreement
+                            We have successfully created a new project for you with the following API KEY:
                           </Typography>
+                          <TextField
+                            autoFocus
+                            margin="dense"
+                            id="apikey"
+                            label="API KEY"
+                            type="text"
+                            value='--'
+                            fullWidth
+                          />
                         </Grid>
                       </Grid>
                     </Paper>
@@ -438,10 +410,14 @@ PLEASE NOTE: We reserve the right, at our sole discretion, to change, modify or 
                             Congratulations <span role="img" aria-label="conrats emoji">🎉</span>
                           </Typography>
                           <Typography variant="body1" gutterBottom>
-                            We have now a positive response
-                          </Typography>
+                            Your project is ready for use please download the SDK for the platforms you need using the links below
+                          </Typography> 
                           <Button fullWidth variant='outlined'>
-                            Download the service invoice or whatever
+                            Download IOS SDK
+                          </Button>
+                          <br /> <br />
+                          <Button fullWidth variant='outlined'>
+                            Download Android SDK
                           </Button>
                         </Grid>
                       </Grid>
@@ -479,4 +455,18 @@ PLEASE NOTE: We reserve the right, at our sole discretion, to change, modify or 
   }
 }
 
-export default withRouter(withStyles(styles)(ProjectAdd));
+
+const mapStateToProps = (state) => ({
+    user: state.user
+  })
+  
+const mapDispatchToProps = (dispatch) => ({
+      setActiveApp: (appid, appname) => dispatch(setActiveApp(appid, appname)),
+})
+  
+export default compose(
+    connect(mapStateToProps, mapDispatchToProps),
+    withRouter,
+    withStyles(styles),
+)(ProjectAdd);
+  
