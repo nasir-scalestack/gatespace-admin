43155ece0ddea8e55f08df03fd3c04721c6948e9
Nasir Dowlatkhahi <ndowlat@Nasirs-MacBook-Air.local>
2019-04-30 03:04:10 -0400

    Add more data

--- src/App.js
+++ src/App.js
@@ -1,43 +1,39 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { ConnectedRouter } from 'connected-react-router'
+import { ConnectedRouter } from 'connected-react-router';
 
 import './App.css';
-import Routes from './routes'
-import store, { history }from './redux/store'
+import Routes from './routes';
+import store, { history } from './redux/store';
 
-import "./assets/css/material-dashboard-react.css?v=1.6.0";
+import './assets/css/material-dashboard-react.css?v=1.6.0';
 
 const theme = createMuiTheme({
   palette: {
     secondary: {
-      main: '#21252e'
+      main: '#21252e',
     },
     primary: {
-      main: '#fa573a'
-    }
+      main: '#fa573a',
+    },
   },
   typography: {
     // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      '"Lato"',
-      'sans-serif'
-    ].join(',')
-  }
+    fontFamily: ['"Lato"', 'sans-serif'].join(','),
+  },
 });
-
 
 class App extends Component {
   render() {
     return (
-        <Provider store={store}>
-          <ConnectedRouter history={history}>
-            <MuiThemeProvider theme={theme}>
-              <Routes />
-            </MuiThemeProvider>
-          </ConnectedRouter>
-        </Provider>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <MuiThemeProvider theme={theme}>
+            <Routes />
+          </MuiThemeProvider>
+        </ConnectedRouter>
+      </Provider>
     );
   }
 }
--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -1,104 +1,176 @@
-import React from "react";
+import React from 'react';
 // @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
+import withStyles from '@material-ui/core/styles/withStyles';
 // core components
 import Grid from '@material-ui/core/Grid';
 // core components
+import CssBaseline from '@material-ui/core/CssBaseline';
+import firebase from '../firebase';
 import Card from './Card/Card';
 import CardHeader from './Card/CardHeader';
 import CardBody from './Card/CardBody';
 
-import GridItem from "./Grid/GridItem";
-import GridContainer from "./Grid/GridContainer";
-import CssBaseline from '@material-ui/core/CssBaseline';
+import GridItem from './Grid/GridItem';
+import GridContainer from './Grid/GridContainer';
 
-import Table from "./Table/Table";
+import Table from './Table/Table';
 
 import Topbar from './Topbar';
+
 const backgroundShape = require('../assets/img/shape.svg');
 
 const styles = theme => ({
-    root: {
-        flexGrow: 1,
-        backgroundColor: theme.palette.grey['100'],
-        overflow: 'hidden',
-        background: `url(${backgroundShape}) no-repeat`,
-        backgroundSize: 'cover',
-        backgroundPosition: '0 400px',
-        paddingBottom: 200,
-        paddingTop: 40
-      },
-   cardCategoryWhite: {
-    "&,& a,& a:hover,& a:focus": {
-      color: "rgba(255,255,255,.62)",
-      margin: "0",
-      fontSize: "14px",
-      marginTop: "0",
-      marginBottom: "0"
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.grey['100'],
+    overflow: 'hidden',
+    background: `url(${backgroundShape}) no-repeat`,
+    backgroundSize: 'cover',
+    backgroundPosition: '0 400px',
+    paddingBottom: 200,
+    paddingTop: 40,
+  },
+  cardCategoryWhite: {
+    '&,& a,& a:hover,& a:focus': {
+      color: 'rgba(255,255,255,.62)',
+      margin: '0',
+      fontSize: '14px',
+      marginTop: '0',
+      marginBottom: '0',
     },
-    "& a,& a:hover,& a:focus": {
-      color: "#FFFFFF"
-    }
+    '& a,& a:hover,& a:focus': {
+      color: '#FFFFFF',
+    },
   },
   cardTitleWhite: {
-    color: "#FFFFFF",
-    marginTop: "0px",
-    minHeight: "auto",
-    fontWeight: "300",
+    color: '#FFFFFF',
+    marginTop: '0px',
+    minHeight: 'auto',
+    fontWeight: '300',
     fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-    marginBottom: "3px",
-    textDecoration: "none",
-    "& small": {
-      color: "#777",
-      fontSize: "65%",
-      fontWeight: "400",
-      lineHeight: "1"
-    }
+    marginBottom: '3px',
+    textDecoration: 'none',
+    '& small': {
+      color: '#777',
+      fontSize: '65%',
+      fontWeight: '400',
+      lineHeight: '1',
+    },
   },
   grid: {
     width: 1550,
     margin: `0 ${theme.spacing.unit * 2}px`,
     [theme.breakpoints.down('sm')]: {
-      width: 'calc(100% - 20px)'
-    }
-  }
+      width: 'calc(100% - 20px)',
+    },
+  },
 });
 
-function UserProfile(props) {
-  const { classes } = props;
-  return (
-        <React.Fragment>
+class UserProfile extends React.Component {
+  state = {
+    events: [],
+  };
+
+  componentDidMount() {
+    firebase
+      .database()
+      .ref('events')
+      .on('child_added', snapshot => {
+        console.log(snapshot.val());
+        const space =
+          snapshot.val().uuid === 'E2C56DB5-DFFB-48D2-B060-D0F5A71096E0'
+            ? 'Toronto Airport'
+            : 'Denver Airport';
+
+        let gate = '';
+
+        if (snapshot.val().major === 1) {
+          gate = 'Gate 1';
+        }
+        if (snapshot.val().major === 2) {
+          gate = 'Gate 2';
+        }
+        if (snapshot.val().major === 3) {
+          gate = 'Gate 3';
+        }
+        if (snapshot.val().major === 4) {
+          gate = 'Gate 4';
+        }
+        if (snapshot.val().major === 5) {
+          gate = 'Gate 5';
+        }
+        const action =
+          snapshot.val().time === 'immediate' ? 'IN_APP_NOTIFICATION' : '';
+        const newEvent = [
+          'Nasir Dowlatkhahi',
+          'Email',
+          snapshot.val().proximity,
+          gate,
+          space,
+          'No Flight',
+          'No Desintation',
+          snapshot.val().time,
+          snapshot.val().time,
+          action,
+        ];
+
+        this.setState(prevState => ({
+          events: [...prevState.events, newEvent],
+        }));
+      });
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <React.Fragment>
         <CssBaseline />
         <Topbar />
         <div className={classes.root}>
-      <GridContainer container justify="center">
-       <Grid spacing={24} alignItems="center" justify="center" container className={classes.grid}>
-       <GridItem xs={12} sm={12} md={12}>
-        <Card>
-          <CardHeader color="primary">
-            <h4 className={classes.cardTitleWhite}>Gate Traffic</h4>
-            <p className={classes.cardCategoryWhite}>
-              All airline data is grabbed from https://aviation-edge.com/
-            </p>
-          </CardHeader>
-          <CardBody>
-            <Table
-              tableHeaderColor="primary"
-              tableHead={["Name", "User Source", "Provider", "Gate", "Flight #", "Flight Destination", "Gate Enter", "Gate Exit", "Action"]}
-              tableData={[
-                ["Bobby Bobby", "Facebook", "Denver Airport", "Gate 5", "JPN-123", "Japan", "April 22 @ 2:51 pm", "April 22 @ 4:51 pm", "PUSH_NOTIFICATION"],
-                ["Nasir Dowlatkhahi", "Email", "Denver Airport", "Gate 3", "AUS-45F", "Australia", "April 19 @ 1:00 pm", "April 19 @ 4:51 pm", "SMS"],
-                ["Armin Khan", "Google Plus", "Denver Airport", "Gate 1", "IRN-445", "Iran", "April 15 @ 5:00 am", "April 15 @ 12:09 pm", "PUSH_NOTIFICATION"],
-
-              ]}
-            />
-          </CardBody>
-        </Card>
-      </GridItem>
-        </Grid>
-      </GridContainer>
-      </div>
-  </React.Fragment>  );
+          <GridContainer container justify="center">
+            <Grid
+              spacing={24}
+              alignItems="center"
+              justify="center"
+              container
+              className={classes.grid}
+            >
+              <GridItem xs={12} sm={12} md={12}>
+                <Card>
+                  <CardHeader color="primary">
+                    <h4 className={classes.cardTitleWhite}>Gate Traffic</h4>
+                    <p className={classes.cardCategoryWhite}>
+                      All airline data is grabbed from
+                      https://aviation-edge.com/
+                    </p>
+                  </CardHeader>
+                  <CardBody>
+                    <Table
+                      tableHeaderColor="primary"
+                      tableHead={[
+                        'Name',
+                        'User Source',
+                        'Proximity',
+                        'Gate',
+                        'Airport',
+                        'Flight #',
+                        'Flight Destination',
+                        'Gate Enter',
+                        'Gate Exit',
+                        'Action',
+                      ]}
+                      tableData={[...this.state.events]}
+                    />
+                  </CardBody>
+                </Card>
+              </GridItem>
+            </Grid>
+          </GridContainer>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default withStyles(styles)(UserProfile);
